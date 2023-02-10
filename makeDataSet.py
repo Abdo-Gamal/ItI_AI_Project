e764e5e727eeb0bee05000efa8580725a5c99f93
Abdulrhman Gamal <60278530+Abdo-Gamal@users.noreply.github.com>
2023-02-10 15:58:23 +0200

    end in toknizer section

--- makeDataSet.py
+++ makeDataSet.py
@@ -18,16 +18,19 @@ dataset_as_string=stransulate_DF_to_string(origin_data_set)
 # print(f"dataset_as_string = {dataset_as_string}")
 # print("\n")
 # print("\n")
-
 #                                                prepareDataSet
 dataset_as_string=dataset_as_string.lower()
-
 dataset_as_string=pp.remove_punctuation(dataset_as_string)
 dataset_as_string=pp.remove_stopwords(dataset_as_string)
 dataset_as_string=pp.stem_words(dataset_as_string)
 dataset_as_string =pp.DeEmojify(dataset_as_string)
-
-print(dataset_as_string)
-
+# print(dataset_as_string)
 #                                                extract futures from dataset
 tokens=dataset_as_string.split()
+# print(f" tokens = {tokens}")
+vocab=fe.Uniqe( tokens)
+# print(f" vocab = {vocab}")
+future=fe.vectorize(tokens)
+print(f" future = {future}")
+
+
